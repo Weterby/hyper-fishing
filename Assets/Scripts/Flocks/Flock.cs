35e61f11c0ccf9ae249e69e21d76070d064bee5e
Adam Schwenk <weterby2@gmail.com>
2021-12-11 17:32:00 +0100

    Add  public and private fields for flock setup

--- Assets/Scripts/Flocks/Flock.cs
+++ Assets/Scripts/Flocks/Flock.cs
@@ -4,15 +4,67 @@ using UnityEngine;
 
 public class Flock : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    #region Constant Fields
+    const float AgentDensity = 0.08f;
+    #endregion
+
+    #region Serialize Fields
+    [SerializeField]
+    private FlockAgent agentPrefab;
+
+    [SerializeField]
+    private FlockBehaviour flockBehaviour;
+
+    [SerializeField, Range(10, 500)]
+    private int startingCount = 250;
+    #endregion
+
+    #region Private Fields
+    private List<FlockAgent> flockAgents = new List<FlockAgent>();
+    private float squareMaxSpeed;
+    private float squareNeighbourRadius;
+    private float squareAvoidanceRadius;
+    #endregion
+
+
+    #region Public Fields
+    [Range(1f, 100f)]
+    public float driveFactor = 10f;
+
+    [Range(1f, 100f)]
+    public float maxSpeed = 5f;
+
+    [Range(1f, 10f)]
+    public float neighbourRadius = 1.5f;
+
+    [Range(0f, 1f)]
+    public float avoidanceRadiusMultiplier = 0.5f;
+
+    public float SquareAvoidanceRadius => squareAvoidanceRadius; 
+    #endregion
     void Start()
     {
-        
+        squareMaxSpeed = maxSpeed * maxSpeed;
+        squareNeighbourRadius = neighbourRadius * neighbourRadius;
+        squareAvoidanceRadius = squareNeighbourRadius * avoidanceRadiusMultiplier * avoidanceRadiusMultiplier;
+
+        for (int i = 0; i < startingCount; i++)
+        {
+            FlockAgent newAgent = Instantiate(
+                agentPrefab,
+                Random.insideUnitCircle * startingCount * AgentDensity,
+                Quaternion.Euler(Vector3.forward * Random.Range(0f, 360f)),
+                transform
+                );
+            newAgent.name = "Agent " + i;
+            flockAgents.Add(newAgent);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Scripts/Flocks/FlockBehaviour.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FlockBehaviour : ScriptableObject
+{
+    public abstract Vector2 CalculateMove(FlockAgent flockAgent, List<Transform> context, Flock flock);
+
+}
