485c798f06d012a3562ecfb3ee2704b6162cae2a
Adam Schwenk <weterby2@gmail.com>
2021-12-11 17:31:22 +0100

    Rename FlockBehavior.cs

--- Assets/Scripts/Flocks/FlockBehavior.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public abstract class FlockBehavior : ScriptableObject
-{
-    public abstract Vector2 CalculateMove(FlockAgent flockAgent, List<Transform> context, Flock flock);
-
-}
