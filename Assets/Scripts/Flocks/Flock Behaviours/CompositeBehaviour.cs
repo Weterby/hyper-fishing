7062963969b70631e6fc6e4fa0f807ef1eebb359
Adam Schwenk <weterby2@gmail.com>
2021-12-12 21:45:53 +0100

    Add public properties and rename fields

--- Assets/Scripts/Flocks/Flock Behaviours/CompositeBehaviour.cs
+++ Assets/Scripts/Flocks/Flock Behaviours/CompositeBehaviour.cs
@@ -8,10 +8,14 @@ public class CompositeBehaviour : FlockBehaviour
     [SerializeField]
     private FlockBehaviour[] flockBehaviours;
     [SerializeField]
-    private float[] weights;
+    private float[] behaviourWeights;
+
+    public FlockBehaviour[] FlockBehaviours => flockBehaviours;
+    public float[] BehaviourWeights => behaviourWeights;
+
     public override Vector2 CalculateMove(FlockAgent flockAgent, List<Transform> context, Flock flock)
     {
-        if(weights.Length != flockBehaviours.Length)
+        if(behaviourWeights.Length != flockBehaviours.Length)
         {
             Debug.LogError($"Data mismatch in {name}", this);
             return Vector2.zero;
@@ -21,14 +25,14 @@ public class CompositeBehaviour : FlockBehaviour
 
         for (int i = 0; i < flockBehaviours.Length; i++)
         {
-            Vector2 partialMove = flockBehaviours[i].CalculateMove(flockAgent, context, flock) * weights[1];
+            Vector2 partialMove = flockBehaviours[i].CalculateMove(flockAgent, context, flock) * behaviourWeights[1];
             
             if(partialMove != Vector2.zero)
             {
-                if(partialMove.sqrMagnitude > weights[i] * weights[i])
+                if(partialMove.sqrMagnitude > behaviourWeights[i] * behaviourWeights[i])
                 {
                     partialMove.Normalize();
-                    partialMove *= weights[i];
+                    partialMove *= behaviourWeights[i];
                 }
 
                 move += partialMove;
