f3e90e127233b017957fd639196bd1711c3b02cd
Adam Schwenk <weterby2@gmail.com>
2021-12-12 17:13:32 +0100

    Fill the cohesion behaviour script and create scriptable object

--- Assets/Scripts/Flocks/Flock Behaviours/CohesionBehaviour.cs
+++ Assets/Scripts/Flocks/Flock Behaviours/CohesionBehaviour.cs
@@ -7,6 +7,20 @@ public class CohesionBehaviour : FlockBehaviour
 {
     public override Vector2 CalculateMove(FlockAgent flockAgent, List<Transform> context, Flock flock)
     {
+        if(context.Count == 0)
+        {
+            return Vector2.zero;
+        }
+        Vector2 cohesionMove = Vector2.zero;
+        foreach(Transform item in context)
+        {
+            cohesionMove += (Vector2)item.position;
+        }
+
+        cohesionMove /= context.Count;
+
+        cohesionMove -= (Vector2)flockAgent.transform.position;
+        return cohesionMove;
     }
 
 }
