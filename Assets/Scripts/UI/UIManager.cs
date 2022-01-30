7c21d032529e00a34910f837fb0ee330bcf63527
Adam Schwenk <weterby2@gmail.com>
2022-01-30 02:30:44 +0100

    Add game menu

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -5,9 +5,38 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private Text scoreText;
     [SerializeField] private ScoreData scoreData;
-
+    [SerializeField] private GameObject menuPanel;
+    private bool menuVisible = false;
     private void Update()
     {
         scoreText.text = scoreData.Score.ToString("000");
+
+        if (!Input.GetKeyDown(KeyCode.Escape)) return;
+        
+        if (!menuVisible)
+        {
+            ShowMenu();
+        }
+        else
+        {
+            CloseMenu();
+        }
+    }
+
+    public void ShowMenu()
+    {
+        menuVisible = true;
+        menuPanel.SetActive(true);
+    }
+
+    public void CloseMenu()
+    {
+        menuVisible = false;
+        menuPanel.SetActive(false);
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
